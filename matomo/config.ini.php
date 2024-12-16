c807b8c153567ae8468eae8469a8869514d6d684
Florian Kroeber <florian.kroeber@website-doktor.com>
2024-12-16 11:00:13 +0100

    added config.ini to gitiignore so we do not get database settings in the repo

--- matomo/config.ini.php
+++ matomo/config.ini.php
@@ -2,5 +2,6 @@
 ; file automatically generated or modified by Matomo; you can manually override the default values in global.ini.php by redefining them in this file.
 [General]
 time_before_today_archive_considered_outdated = 1
-installation_first_accessed = 1734342513
+enable_browser_archiving_triggering = 1
+browser_archiving_disabled_enforce = 0
 
